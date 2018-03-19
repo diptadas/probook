adebb27782e321255a82906f253a906f16a6603b
diptadas <dipta670@gmail.com>
2018-03-19 16:23:25 +0600

    Updated haproxy-reload

--- haproxy-reload/test.sh
+++ haproxy-reload/test.sh
@@ -4,10 +4,10 @@ set -eou pipefail
 sed -i -e 's/9080/9090/g' config/haproxy.cfg
 sleep 1
 
-xdg-open http://127.0.0.1:9090/1
+xterm -hold -title "test-1" -e "curl 127.0.0.1:9090/1"
 
 sed -i -e 's/9090/9080/g' config/haproxy.cfg
 sleep 1
 
-xdg-open http://127.0.0.1:9090/2
-xdg-open http://127.0.0.1:9080/3
+xterm -hold -title "test-2" -e "curl 127.0.0.1:9090/2"
+xterm -hold -title "test-3" -e "curl 127.0.0.1:9080/3"
