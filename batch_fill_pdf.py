e92b3976750e5eaef35cc5d46badbff1c4685532
Dipta Das <dipta670@gmail.com>
2021-08-12 18:00:43 -0500

    Create batch_fill_pdf.py

---
+++ batch_fill_pdf.py
@@ -0,0 +1,18 @@
+# !pip3 install fillpdf
+
+from fillpdf import fillpdfs
+
+# fillpdfs.get_form_fields("template.pdf")
+
+names = ['John De', 'Andrew Rob', "Mike Kyle"]
+date = '12 August 2021'
+template = 'template.pdf'
+
+for name in names:
+    data_dict = {
+        'Name': name,
+        'Date': date,
+    }
+    output = 'certificate_' + name + '.pdf'
+    fillpdfs.write_fillable_pdf(template, output, data_dict)
+    fillpdfs.flatten_pdf(output, output)
